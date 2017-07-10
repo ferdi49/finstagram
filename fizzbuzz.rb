724e4be8439f2f47f9e273f28850b32a27aabaca
Ferdinand Widjojo Putra <ferdinand.widjojoputra@mail.mcgill.ca>
2017-07-10 22:57:59 +0000

    This include all changes since week 4

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+(1..100).each do |n|
+    if n%3==0 && n%5==0 
+        puts n
+    elseif n%5==0
+        puts "Buzz"
+    elseif 
+        puts "FizzBuzz"
+    else n % 3 == 0
+        puts "Fizz"
+    end
+end
